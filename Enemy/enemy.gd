65eaebd1caa0cd0f331485002d793195cc623051
MatiasGiraudo <maty.giraudo@hotmail.com>
2024-06-03 00:38:39 -0300

    Modifico comportamiento de enemigo

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -2,119 +2,50 @@ extends CharacterBody3D
 
 class_name Enemy
 
-var speed = 5
-var accel = 10
-var attack_speed_multiplier = 5
-var attacking = false
-var current_state = state.SEEKING
-
-var attack_target: Vector3
-var return_target: Vector3
-
-var default_material = load("res://Enemy/enemyDefaultColor.tres")
-var attack_material = load("res://Enemy/enemyAttackMaterial.tres")
-var resting_material = load("res://Enemy/enemyRestingMaterial.tres")
-
+@export var SPEED = 4.0
+var ATTACK_RANGE = 2.5
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-
-enum state {
-	SEEKING,
-	ATTACKING,
-	RETURNING,
-	RESTING
-}
+var state_machine
 
 @onready var player = $"../Player1" as CharacterBody3D
-@onready var nav: NavigationAgent3D = $NavigationAgent3D
-@onready var attack_timer = $AttackTimer
+@onready var nav_agent: NavigationAgent3D = $NavigationAgent3D
 @onready var attack_audio = $AudioAttack
-@onready var death_audio = $AudioDeath
-
 @onready var anim_tree = $AnimationTree
-#func _ready():
-	#$MeshInstance3D.set_surface_override_material(0, default_material)
 
+func _ready():
+	state_machine = anim_tree.get("parameters/playback")
 
 func _physics_process(delta):
-	var direction = Vector3()
+	velocity = Vector3.ZERO
 	
 	if not is_on_floor():
 		velocity.y -= gravity * delta
+		
+	match state_machine.get_current_node():
+		"Run":
+			nav_agent.set_target_position(player.global_transform.origin)
+			var next_nav_point = nav_agent.get_next_path_position()
+			velocity = (next_nav_point - global_transform.origin).normalized() * SPEED
+			look_at(Vector3(global_position.x + velocity.x, global_position.y, global_position.z + velocity.z), Vector3.UP)
+			
+		"Attack":
+			look_at(Vector3(player.global_position.x, global_position.y, player.global_position.z), Vector3.UP)
 	
-	if is_instance_valid(player):
-		match current_state:
-			state.SEEKING:
-				nav.target_position = player.global_position
-				
-				direction = nav.get_next_path_position() - global_position
-				direction = direction.normalized()
-				
-				velocity = velocity.lerp(direction * speed, accel * delta)
-
-				var look_at_me = Vector3(player.global_position.x,global_position.y, player.global_position.z)
-				look_at(look_at_me)
-				
-				move_and_slide()
-				
-				
-				if $AttackRadio.overlaps_body(player):
-					init_attack()
-				
-			state.ATTACKING:
-				move_and_attack()
-			state.RETURNING:
-				move_and_attack()
-			state.RESTING:
-				pass
-
-func move_and_attack():
-	#var direction: Vector3 = player.global_position - global_position
-	var attack_vector: Vector3 = attack_target - global_transform.origin
-	var direction: Vector3 = attack_vector.normalized()
+	anim_tree.set("parameters/conditions/attack", target_in_range())
+	anim_tree.set("parameters/conditions/run", !target_in_range())
 	
-	velocity = direction * speed * attack_speed_multiplier
 	move_and_slide()
-	attack_audio.play()
 	
-	
-	var distance = attack_vector.length()
-	
-	if distance <= 1.1:
-		match current_state:
-			state.ATTACKING:
-				anim_tree.set("parameters/conditions/attack", true)
-				var stats_player : Stats = player.get_node("Stats")
-				stats_player.take_hit(1)
-				
-				current_state = state.RETURNING
-				attack_target = return_target
-			state.RETURNING:
-				anim_tree.set("parameters/conditions/run", true)
-				current_state = state.RESTING
-				$MeshInstance3D.set_surface_override_material(0, resting_material)
-				collide_with_other_enemies(true)
-				direction = Vector3.ZERO
-				move_and_slide()
-				attack_timer.start()
 
-func collide_with_other_enemies(should_be_collide):
-	set_collision_mask_value(3, should_be_collide)
-	set_collision_layer_value(3, should_be_collide)
+func target_in_range():
+	return global_position.distance_to(player.global_position) < ATTACK_RANGE
+	
+func hit_finished():
+	var stats_player : Stats = player.get_node("Stats")
+	stats_player.take_hit(1)
+	attack_audio.play()
 
 func _on_stats_dead_signal():
 	anim_tree.set("parameters/conditions/dead", true)
-	current_state = state.RESTING
 	await get_tree().create_timer(4).timeout 
 	queue_free()
-
-func init_attack():
-	return_target = global_transform.origin
-	attack_target = player.global_transform.origin
-	current_state = state.ATTACKING
-	#$MeshInstance3D.set_surface_override_material(0, attack_material)
-	collide_with_other_enemies(false)
-
-
-func _on_attack_timer_timeout():
-	current_state = state.SEEKING
-	#$MeshInstance3D.set_surface_override_material(0, default_material)
